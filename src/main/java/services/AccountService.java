e148b66b6d56e8b4d29435c93d6a826056f22bb1
hideyo_soma <hideyo_soma@trustsystem.co.jp>
2025-06-11 11:31:52 +0900

    アカウントサービスselect追加

--- src/main/java/services/AccountService.java
+++ src/main/java/services/AccountService.java
@@ -2,7 +2,10 @@ package services;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.NamingException;
 
@@ -10,6 +13,33 @@ import beans.Account;
 import utils.Db;
 
 public class AccountService {
+	public List<Account> select(String mail) {
+		List<Account> list = new ArrayList<>();
+				String sql = "SELECT * FROM accounts";
+//				String sql = "SELECT * FROM accounts";
+//				String sql = "SELECT * FROM accounts";
+				try (Connection conn = Db.open();
+			             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			            
+			            pstmt.setString(1, mail);
+			            ResultSet rs = pstmt.executeQuery();
+
+			            while (rs.next()) {
+			                Account a = new Account(
+			                    rs.getInt("account_id"),
+			                    rs.getString("name"),
+			                    rs.getString("mail"),
+			                    rs.getString("password"),
+			                    rs.getInt("authority")
+			                );
+			                list.add(a);
+			            }
+			        } catch (SQLException | NamingException e) {
+			            e.printStackTrace();
+			        }
+			        return list;
+	}
+	
 	public void insert(Account a) {
 		String sql = "INSERT INTO accounts (name, mail, password, authority) VALUES (?, ?, ?, ?)";
 		try (Connection conn = Db.open(); 
@@ -50,4 +80,52 @@ public class AccountService {
             e.printStackTrace();
         }
     }
+	public Account findById(int id) {
+        String sql = "SELECT * FROM accounts WHERE id = ?";
+        try (Connection conn = Db.open();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Account a = new Account(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("mail"),
+                    rs.getString("password"),
+                    rs.getInt("authority")
+                );
+                return a;
+            }
+        } catch (SQLException | NamingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+	
+	public Account findByAccount(String name, String mail, int authority) {
+	    String sql = "SELECT * FROM accounts WHERE name = ? AND mail = ? AND authority = ?";
+	    try (Connection conn = Db.open();
+	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+	        pstmt.setString(1, name);
+	        pstmt.setString(2, mail);
+	        pstmt.setInt(3, authority);
+
+	        ResultSet rs = pstmt.executeQuery();
+	        if (rs.next()) {
+	            return new Account(
+	                rs.getInt("id"),
+	                rs.getString("name"),
+	                rs.getString("mail"),
+	                rs.getString("password"),
+	                rs.getInt("authority")
+	            );
+	        }
+	    } catch (SQLException | NamingException e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
+
 }
